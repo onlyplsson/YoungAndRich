b82707009fc844c8abdf57a8681b427e80d6efce
onlyplsson <onlyplsson@gmail.com>
2022-05-16 19:09:39 +0900

    메뉴 목록 띄우기 전까지

--- routes/vote.js
+++ routes/vote.js
@@ -11,7 +11,7 @@ router.use(bodyParser.urlencoded({ extends: false }));
 
 router.get("/", function (req, res) {
   try {
-    var sql = `select count(*) as total from VOTE;`;
+    var sql = `select count(menu_name) as total from VOTE;`;
     conn.query(sql, (err, rows, fields) => {
       var total = rows[0].total;
       res.render(path.join(__dirname, "../views/vote.ejs"), {
@@ -28,12 +28,13 @@ router.post("/", function (req, res) {
     var user = req.body.user;
     var choice = req.body.choice;
 
-    var sql = `insert into VOTE values ('${user}', '${choice}');`;
+    var sql = `update VOTE set menu_name='${choice}' where user_name='${user}';`;
     conn.query(sql, (err, rows, fields) => {
       if (err) {
         res.send(
           "<script>alert('이미 투표를 하셨습니다.');location.href='../vote/';</script>"
         );
+        console.log(err);
       } else {
         res.send(
           "<script>alert('투표 완료');location.href='../vote';</script>"
@@ -62,7 +63,7 @@ router.get("/result", function (req, res) {
   }
 });
 
-router.get("/menu", function (req, res) {
+router.get("/order", function (req, res) {
   try {
     res.render(path.join(__dirname, "../views/menu.ejs"));
   } catch (err) {
@@ -70,9 +71,27 @@ router.get("/menu", function (req, res) {
   }
 });
 
+router.post("/order/save", function (req, res) {
+  try {
+    var user = req.body.user;
+    var menu = req.body.menu;
+
+    var sql = `update CHOICE set order_name='${menu}' where user_name='${user}';`;
+    conn.query(sql, (err, rows, fields) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("<script>alert('저장 완료');location.href='../';</script>");
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 router.post("/reset", function (req, res) {
   try {
-    var sql = `delete from VOTE;`;
+    var sql = `update VOTE set menu_name = NULL;`;
     conn.query(sql, (err, rows, fields) => {
       if (err) {
         console.log(err);
